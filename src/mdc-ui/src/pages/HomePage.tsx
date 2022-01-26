49ebcfbdeb71e8f179f91c0b41ff24acd0f6a11d
jittaearn <jitta.k@ku.th>
2022-01-26 22:24:08 +0700

    Add antd & created an upload button & Fix dependencies

--- src/mdc-ui/src/pages/HomePage.tsx
+++ src/mdc-ui/src/pages/HomePage.tsx
@@ -1,13 +1,33 @@
 import React from 'react';
 
 import { Link } from 'react-router-dom';
+import { Upload, message, Button } from 'antd';
+import { UploadOutlined } from '@ant-design/icons';
 
 const HomePage: React.FC = () => {
+  const uploadProps = {
+    name: 'file',
+    // add API to store the uploaded file
+    action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
+    headers: {
+      authorization: 'authorization-text',
+    },
+    onChange(info: any) {
+      if (info.file.status !== 'uploading') {
+        console.log(info.file, info.fileList);
+      }
+      if (info.file.status === 'done') {
+        message.success(`${info.file.name} file uploaded successfully`);
+      } else if (info.file.status === 'error') {
+        message.error(`${info.file.name} file upload failed.`);
+      }
+    },
+  };
   return (
     <div>
-      <p>Hi, Hello</p>
-      <Link to={'/test'}>Go to TestPage</Link>
-      <p> <a href={'/admin'}>Go to Django Admin</a> </p>
+      <Upload {...uploadProps}>
+        <Button icon={<UploadOutlined />}>Click to Upload</Button>
+      </Upload>,
     </div>
   );
 };
