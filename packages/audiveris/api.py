98a4a6e3c78f30cb8d56c497c7b8aa63c51fe916
Mai Mee <47658413+MaiMee1@users.noreply.github.com>
2022-04-15 22:51:56 +0700

    Add script to merge playlists

--- packages/audiveris/api.py
+++ packages/audiveris/api.py
@@ -6,10 +6,12 @@ import pathlib
 import subprocess
 import time
 import typing
+from copy import deepcopy
 
 import chardet
 import numpy as np
 import pandas as pd
+from lxml import etree
 
 from utils import iter as iterutils
 
@@ -121,6 +123,33 @@ class Audiveris:
     #             logging.error(line)
     #             print(line)
     #     # return output
+
+    def get_playlist(self, input_files: list[str]):
+        playlist = etree.Element("play-list")
+        sheets_selection_1 = etree.Element("sheets-selection")
+        sheets_selection_1.text = "1"
+        for input_file in input_files:
+            excerpt = etree.SubElement(playlist, "excerpt")
+            path = etree.SubElement(excerpt, "path")
+            path.text = input_file
+            excerpt.append(deepcopy(sheets_selection_1))
+        s = etree.tostring(playlist, pretty_print=True)
+        if isinstance(s, bytes):
+            s = s.decode()
+        return f'<?xml version="1.0" ?>\n{s}'
+
+    def save_playlist(self, filename: typing.Union[str, pathlib.Path], input_files: list[str]):
+        s = self.get_playlist(input_files)
+        with open(filename, 'w') as f:
+            f.write(s)
+
+    def export_playlist_args(self, playlist_path: str):
+        return self.with_args(
+            "-batch",
+            "-output", self.output_dir,
+            "-playlist", playlist_path,
+            "-save",
+        )
 
 
 async def run(program, *args):
@@ -252,15 +281,10 @@ async def export_mxl_async(app_home: str, output_dir: str, data_dir: pathlib.Pat
     await asyncio.gather(*worker_tasks, record_task, return_exceptions=True)
 
 
-def export_mxl(app_home: str, output_dir: str, data_dir: pathlib.Path, *, load: bool = True, use_omr: bool = True, start_at: int = 1, end_at: int = float('inf')):
+def export_mxl(app_home: str, output_dir: str, data_dir: pathlib.Path, *, use_omr: bool = True, start_at: int = 1, end_at: int = float('inf')):
     seconds_per_page = np.fromiter([], int)
     df_staff = pd.read_csv(data_dir / "henle-images-no-staff.csv")
-    if load:
-        df_exported = pd.read_csv(data_dir / "henle-images-exported.csv")
-        df = df_staff.merge(df_exported, how='left', on=['hn', 'page']).set_index(['hn', 'page'])
-    else:
-        df_exported = pd.DataFrame([], columns=['hn', 'page', 'exported'])
-        df = df_staff.set_index(['hn', 'page'])
+    df_indexed = df_staff.set_index(['hn', 'page'])
 
     for hn_path in (data_dir / "henle").glob("*"):
         hn = hn_path.stem
@@ -268,11 +292,8 @@ def export_mxl(app_home: str, output_dir: str, data_dir: pathlib.Path, *, load: 
             continue
         audiveris = Audiveris(app_home=app_home, output_dir=f"{output_dir}\\{hn:0>4}")
 
-        x: pd.DataFrame = df.loc[int(hn)]
-        if load:
-            pages = x[(x['has_staff'] == 1) & (x['exported'] != 1)].index
-        else:
-            pages = x[x['has_staff'] == 1].index
+        x: pd.DataFrame = df_indexed.loc[int(hn)]
+        pages = x[x['has_staff'] == 1].index
 
         if len(pages) == 0:
             print(f"ERROR\t\t[] HN {hn} has no notes pages, skipping")
@@ -314,13 +335,86 @@ def export_mxl(app_home: str, output_dir: str, data_dir: pathlib.Path, *, load: 
             print(stderr)
             logging.error(stderr)
 
-        exported = 1 if proc.returncode == 0 else 0
-        for page in pages:
-            df_exported.loc[df.shape[0]] = {'hn': hn, 'page': page, 'exported': exported}
         start_time = time.time_ns()
-        df.to_csv(data_dir / "henle-images-exported.csv", index=False)
+        df_exported = get_df_exported(output_dir)
+        df_exported.to_csv(data_dir / "henle-images-exported.csv", index=False)
         elapsed = time.time_ns() - start_time
         print(f"INFO\t\t[] HN {hn} saved to `henle-images-exported.csv` ({elapsed} nanoseconds)")
+
+
+def get_df_exported(output_dir: str) -> pd.DataFrame:
+    df = pd.DataFrame([], columns=['hn', 'page', 'num_mxl'])
+    top_path = pathlib.Path(output_dir)
+    for hn_path in top_path.glob("*"):
+        if not hn_path.is_dir():
+            continue
+        hn = hn_path.stem
+        print(hn, end='')
+        for page_path in hn_path.glob("*"):
+            if not page_path.is_dir():
+                continue
+            page = page_path.stem
+            df.loc[df.shape[0]] = {'hn': hn, 'page': page, 'num_mxl': sum(1 for _ in page_path.glob("*.mxl"))}
+            print('.', end='')
+        print()
+    return df
+
+
+def save_compound_book(app_home: str, output_dir: str, *, start_at: int = 1, end_at: int = float('inf')):
+    s_start_time = time.time_ns()
+    df = pd.read_csv(data_dir / "henle-images-exported.csv")
+    df_indexed = df[df["num_mxl"] == 1].set_index(["hn", "page"])
+
+    ns_per_page = np.fromiter([], int)
+    for hn in df_indexed.groupby(['hn']).indices.keys():
+        if hn < start_at or hn > end_at:
+            continue
+        # audiveris = Audiveris(app_home=app_home, output_dir=f"{output_dir}\\{hn:0>4}")
+        audiveris = Audiveris(app_home=app_home, output_dir=f"{data_dir}\\playlists\\{hn:0>4}")
+
+        pages = df_indexed.loc[hn].index
+        print(f"INFO\t\t[] HN {hn} starting... time is {time.asctime()} (ETA is {'???' if ns_per_page.size == 0 else time.ctime(time.time() + np.percentile(ns_per_page, 70, overwrite_input=True)/(10 ** 9))})")
+        start_time = time.time_ns()
+        omr_files = [f"{output_dir}\\{hn:0>4}\\{page:0>4}\\{page:0>4}.omr" for page in pages]
+        tmp = f"{data_dir}\\playlists\\{hn:0>4}.xml"
+        audiveris.save_playlist(tmp, input_files=omr_files)
+        print(f"INFO\t\t[] saved {tmp}")
+        args = audiveris.export_playlist_args(tmp)
+        proc = subprocess.run(args, timeout=5*60)
+        elapsed = time.time_ns() - start_time
+
+        ns_per_page = np.append(ns_per_page, elapsed / len(pages))
+        if proc.returncode == 0:
+            # INFO  []                      Book 1485 | Loading book D:\data\MDC\audiveris\0002\0165\0165.omr
+            # INFO  []                  PlayList 113  | BookExcerpt{0008 spec:1}
+            # INFO  []                  PlayList 129  |    Processing Stub#1
+            # INFO  []                  PlayList 147  | Copying tree from D:\data\MDC\audiveris\0002\0164\0164.omr/sheet#1 to D:\data\MDC\playlists\0002.omr/sheet#125
+            # INFO  []                  PlayList 113  | BookExcerpt{0165 spec:1}
+            # INFO  []                  PlayList 129  |    Processing Stub#1
+            # INFO  []                  PlayList 147  | Copying tree from D:\data\MDC\audiveris\0002\0165\0165.omr/sheet#1 to D:\data\MDC\playlists\0002.omr/sheet#126
+            # INFO  []                      Book 2056 | Stored /book.xml
+            # INFO  []                  PlayList 182  | Compound book created as D:\data\MDC\playlists\0002.omr
+            # stdout = proc.stdout.decode(encoding=chardet.detect(proc.stdout).get('encoding') or 'windows-1252')
+            # for line in stdout.splitlines():
+            #     if 'Compound book created' in line:
+            #         print(line)
+            print(f"INFO\t\t[] HN {hn} processed {len(pages):>3} pages, ??? exported"
+                  f" ({elapsed // (10**9) // 60} minutes {elapsed // (10**9) % 60} seconds) [{ns_per_page[-1] // (10 ** 6)} ms per page]")
+        else:
+            print(f"ERROR\t\t[] HN {hn} (code: {proc.returncode})")
+            # stdout = proc.stdout.decode(encoding=chardet.detect(proc.stdout).get('encoding') or 'windows-1252')
+            # print(stdout)
+            # logging.debug(stdout)
+            # stderr = proc.stderr.decode(encoding=chardet.detect(proc.stderr).get('encoding') or 'utf-16')
+            # print(stderr)
+            # logging.error(stderr)
+    print(f"\n"
+          f"Tasked finished at {time.asctime()}\n"
+          f"elapsed: {(time.time_ns() - s_start_time) // 10**9} s\n"
+          f"total books: {len(ns_per_page)}\n"
+          f"total time: {np.sum(ns_per_page) // 10**9} s\n"
+          f"median+ time: {np.percentile(ns_per_page, 60, overwrite_input=True) // (10 ** 6)} ms per page\n"
+          f"average time: {np.mean(ns_per_page) // (10 ** 6)} ms per page")
 
 
 if __name__ == '__main__':
@@ -330,5 +424,6 @@ if __name__ == '__main__':
 
     # process_staffs(app_home, output_dir, data_dir)
     # asyncio.run(export_mxl_async(app_home, output_dir, data_dir, batch_size=5, num_workers=1, max_qsize=1, load=False, use_omr=False))
-    # export_mxl(app_home, output_dir, data_dir, load=False, use_omr=False, start_at=9423, end_at=9423)
-    # export_mxl(app_home, output_dir, data_dir, load=False, use_omr=False, start_at=1491)
+    # export_mxl(app_home, output_dir, data_dir, use_omr=False, start_at=9423, end_at=9423)
+    # export_mxl(app_home, output_dir, data_dir, use_omr=False, start_at=1491)
+    # save_compound_book(app_home, output_dir, start_at=650)
