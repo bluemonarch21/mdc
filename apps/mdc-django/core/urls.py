ffc6ee1f40cd636bb47334727ae6e2e61cf50eab
Mai Mee <47658413+MaiMee1@users.noreply.github.com>
2022-04-19 15:44:45 +0700

    Remove mdc-django and wrongly committed folder

--- apps/mdc-django/core/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, re_path
-
-from . import views
-
-app_name = "core"
-urlpatterns = [
-    path("upload", views.upload_file, name="upload"),
-    re_path(r"^(?!admin).*", views.index, name="index"),
-]
